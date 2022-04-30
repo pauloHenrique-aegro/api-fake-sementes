99b7cec983972d6abae49fcc697890b99f702094
pauloHenrique-aegro <paulo.santos@aegro.com.br>
2022-04-30 10:57:55 -0300

    Alterações na tabela seed

--- create_table.sql
+++ create_table.sql
@@ -2,7 +2,5 @@ create table seed(
     id serial,
     seed_name text not null,
     days_to_germinate integer not null,
-  	created_at timestamp default NOW(),
- 	sync_date timestamp,
   	sync boolean default false
 )
